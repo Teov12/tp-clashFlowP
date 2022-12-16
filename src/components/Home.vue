d5ac7f29e0a721cd9d2309bee85ba7d09fde11a2
Teo Camusso <teocamusso29@gmail.com>
2022-12-16 12:07:27 -0300

    graficos

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,41 +6,101 @@ import Movements from "./Movements/Index.vue"
 import Action from "./Action.vue"
 import Graphic from "./Resume/Graphic.vue"
 import { ref } from "@vue/reactivity";
+import { computed } from "@vue/runtime-core";
 
 const monto = ref(null)
 const label = ref(null)
-const mounts = ref([100, 200, 500, 200, -400, -500, -300, 0, 300, 500])
 const movements = ref([{
           id: 0,
-          title: "Movimiento",
+          title: "Movimiento 1",
           description: "Deposito de salario",
-          amount: 1000,
+          amount: 100,
+          time: new Date("11-12-2022"),
         },
         {
           id: 1,
-          title: "Movimiento 1",
+          title: "Movimiento 2",
           description: "Deposito de honorarios",
-          amount: 500,
+          amount: 200,
+          time: new Date("11-12-2022"),
         },
         {
           id: 2,
           title: "Movimiento 3",
           description: "Comida",
-          amount: -100,
+          amount: 500,
+          time: new Date("12-12-2022"),
         },
         {
           id: 3,
           title: "Movimiento 4",
           description: "Colegiatura",
-          amount: 1000,
+          amount: 200,
+          time: new Date("13-12-2022"),
         },
         {
           id: 4,
           title: "Movimiento 5",
           description: "Reparación equipo",
-          amount: 1000,
+          amount: -400,
+          time: new Date("14-12-2022"),
         },
+        {
+          id: 5,
+          title: "Movimiento 6",
+          description: "Reparación equipo",
+          amount: -600,
+          time: new Date("15-12-2022"),
+        },
+        {
+          id: 6,
+          title: "Movimiento 7",
+          description: "Reparación equipo",
+          amount: -300,
+          time: new Date("10-12-2022"),
+        },
+        {
+          id: 7,
+          title: "Movimiento 8",
+          description: "Reparación equipo",
+          amount: 0,
+          time: new Date("9-12-2022"),
+        },
+        {
+          id: 8,
+          title: "Movimiento 9",
+          description: "Reparación equipo",
+          amount: 300,
+          time: new Date("13-12-2022"),
+        },
+        {
+          id: 9,
+          title: "Movimiento 10",
+          description: "Reparación equipo",
+          amount: 500,
+          time: new Date("12-12-2022"),
+        },
+
 ]);
+
+const amounts = computed(()=>{
+  const lastDays = this.movements
+  .filter(a => {
+    const today = new Date();
+    const oldDate = today.setDate(today.getDate() - 30);
+
+    return a.time > oldDate;
+  })
+  .map(a=>a.amount)
+
+  return lastDays.map((a, i) =>{
+    const lastMovements =  lastDays.slice(0, i);
+
+    return lastMovements.reduce((suma, movement)=>{
+      return suma + movement;
+    }, 0);
+  })
+})
 </script>
 
 <template>
@@ -51,14 +111,14 @@ const movements = ref([{
 
     <template #resume>
       <Resume 
-        :labeltotal="label"
-        label="Ahorro total" 
-        :monto= "monto" 
-        montototal= 10000002>
-
+          label="Ahorro total" 
+          :labeltotal="label"
+          :monto= "monto" 
+          montototal= 10000002>
+        
         <template #graphic>
           <Graphic
-          :amounts="amounts"
+            :amounts="amounts"
           />
         </template>
 
@@ -69,7 +129,9 @@ const movements = ref([{
     </template>
 
     <template #movements>
-      <Movements :movements="movements"></Movements>
+      <Movements 
+        :movements="movements">
+      </Movements>
     </template>
 
   </Layout>
--- src/components/Resume/Graphic.vue
+++ src/components/Resume/Graphic.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { defineProps, toRefs, computed } from 'vue';
+import { defineProps, toRefs, computed, ref, defineEmits } from 'vue';
 
 const props = defineProps({
         amounts: {
@@ -9,21 +9,69 @@ const props = defineProps({
     
 })
 
-const {amounts} = toRefs(props)
+const { amounts } = toRefs(props)
+
+const amountToPixels = (amount) =>{
+    const min = Math.min(...amounts.value);
+    const max = Math.max(...amounts.value);
+
+    const amountAbs = amount + Math.abs(min);
+    const minMax = Math.abs(max) + Math.abs(min);
+
+    return 200 - ((amountAbs *100) / minMax) * 2;
+
+    
+}
+
+const points = computed(() => {
+    const total = amounts.value.length;
+    return amounts.value.reduce((points, amount, i)=>{
+        const x = (300/total) * (i + 1);
+        const y = amountToPixels(amount);
+        console.log(y);
+        return `${points} ${x}, ${y}`;
+    }, "0, 100")
+});
+
+const zero = computed(()=>{
+    return amountToPixels(0);
+});
+
+const showPointer = ref(false);
+const pointer = ref(0);
+
+const emit = defineEmits(["select"])
+
+const tap = ({target, touches}) => {
+    showPointer.value = true;
+    const elementWidth = target.getBoundingClientRect().width;
+    const elementX = target.getBoundingClientRect().x;
+    const touchX = touches[0].clientX;
+    pointer.value = ((touchX - elementX) * 300) / elementWidth;
+    emit("select", monto)
+};
+
+const untap = () =>{
+    showPointer.value = false;
+}
+
 </script>
 
 <template>
     <div>
         <svg
+            @touchstart="tap"
+            @touchmove="tap"
+            @touchend="untap"
             viewBox="0 0 300 200"
         >
             <line
                 stroke="#c4c4c4"
                 stroke-width="2"
                 x1="0"
-                y1="100"
+                :y1="zero"
                 x2="300"
-                y2="100"
+                :y2="zero"
             />
             <polyline
                 fill="none"
@@ -32,11 +80,12 @@ const {amounts} = toRefs(props)
                 :points="points"
             />
             <line
+                v-show="showPointer"
                 stroke="#04b500"
                 stroke-width="2"
-                x1="200"
+                :x1="pointer"
                 y1="0"
-                x2="200"
+                :x2="pointer"
                 y2="200"
             />
         </svg>
